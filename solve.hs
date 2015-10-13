2989aa2732f80857d7197fffcded811d6aaf1e20
vijaykeswani <vijaykeswani.slg@gmail.com>
2015-10-13 17:58:33 +0530

    Adding State monad

--- solve.hs
+++ solve.hs
@@ -1,14 +1,27 @@
 module Satsolve where
 	import Control.Monad
 
-	type Literal = Bool
+	type Variable = String
+
+	data BVariable a = Same a | Negate a
+
+	type Literal = BVariable Variable 
 	type Clause = [Literal]
+
 	data DoubtFul a =True 
 			| False 
 			| Assign [a]
 
 	type Formula = DoubtFul Clause
-
 	type Variables = [String]
+	type Assignment = [(String,Bool)]
+
+	assignValues :: Formula -> Assignment -> (Formula, Assignment)
+	assignValues True _ = (True, _)
+	assignValues False _ = (False, _)
+	assignValues Assign lst assignment = av1 lst assignment
+
+	av1 :: [Clauses] -> assignment -> Formula
 	
+
 	
