cb483830487029c78578f58b1bc10169af261321
Vijay Keswani <vijaykeswani.slg@gmail.com>
2015-11-08 01:15:22 +0530

    Gsat done hopefully. Implementation's a bit rough currently

--- Gsat.hs
+++ Gsat.hs
@@ -1,6 +1,9 @@
 	import Satsolve
 	import Backtrack
 	import qualified Control.Monad.State.Lazy as S
+        import System.Random
+        import System.IO.Unsafe
+        import System.Random.Mersenne.Pure64 (pureMT)
 {-
 	gsatsolve :: Formula -> Int -> Assignment -> Bool
 	gsatsolve formula assignment = 
@@ -12,16 +15,49 @@
 	flipV v ((x,y):xs) | x==v = (x, not y):(flipV v xs)
 			  | otherwise = (x,y):(flipV v xs)
 
-	gsatrun :: Formula -> Int -> Variables -> Assignment -> Assignment -> (Formula, Assignment)
-	gsatrun form n (x:xs) nassgn oassgn 	| n_sat < n = gsatrun form n_sat xs tassgn oassgn
-						| otherwise = gsatrun form n xs nassgn oassgn 
+	gsatrun :: Formula -> Variables -> Assignment -> Assignment -> (Formula, Assignment)
+	gsatrun form (x:xs) nassgn oassgn 	| n_sat <= n = gsatrun form xs tassgn oassgn
+						| otherwise = gsatrun form xs nassgn oassgn 
 		where 	n_sat = getUnsat form tassgn
+                        n = getUnsat form nassgn
 			tassgn = flipV x oassgn	
-	gsatrun form n [] [] oassgn = S.runState stateF oassgn
+	gsatrun form [] [] oassgn = S.runState stateF oassgn
 				where stateF = stateFormula form
-	gsatrun form n [] nassgn oassgn = S.runState stateF nassgn
+	gsatrun form [] nassgn oassgn = S.runState stateF nassgn
 				where stateF = stateFormula form
-	
+
+
+	gsatrunSt :: Formula -> Variables -> Assignment -> S.State Assignment Formula
+        gsatrunSt form lst nassgn = S.state (\ass -> (gsatrun form lst nassgn ass))
+      
+        
+      
+--        gsat :: Formula -> Variables -> Assignment
+        gsat form n var assgn = do
+                gsatrunSt form var assgn 
+                x <- S.get
+                let num = getUnsat form x
+                if(num ==0 || n==0)
+                    then return x
+                    else gsat form (n-1) var x
+    
+--        temp form var lst1 lst2 = y
+--            where (x,y) = S.runState (gsat form 100 var lst1) lst2
+
+
+        getRandomAss :: Int -> [Variable] -> Assignment
+        getRandomAss _ [] = []
+        getRandomAss n (x:xs) = (x,rt):(getRandomAss (n+1) xs)
+            where rt = fst (random $ pureMT (fromInteger $ toInteger n))
+                   --         1 -> True
+                   --         0 -> False
+        
+        runMultiple form var 0 = getRandomAss 33 var
+        runMultiple form var n    | k==0 = y
+                            | otherwise = runMultiple form var (n-1)
+            where   (x,y) = S.runState (gsat form 100 var assgn) assgn
+                    assgn = getRandomAss 22 var
+                    k = getUnsat form y
 
         main = do
                 var <- getLine
@@ -29,12 +65,14 @@
                 form <- getIn (read num)
                 print (bt (Assign form) (words var))
                 print $ printf (Assign form)
-                ass <- getLine
-                let assgn = formA (words var) (words ass)
-                let a = (getUnsat (Assign form) assgn)
-                let (c,b) = gsatrun (Assign form) a (words var) [] assgn
-                let d = (getUnsat (Assign form) b)
-		print a
-		print b
-		print d
-	
+--                ass <- getLine
+--                let assgn = formA (words var) (words ass)
+--                let a = (getUnsat (Assign form) assgn)
+                --let (c,b) = gsatrun (Assign form) (words var) [] assgn
+                --let d = (getUnsat (Assign form) b)
+                let y = runMultiple (Assign form) (words var) 100
+		--print a
+		--print b
+		--print d
+	        print y
+            
